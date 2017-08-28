e6fb55c8172f3018587588ea595fb1e00623430a
Ralf Waldukat <ralf.waldukat+git@gmail.com>
2017-08-28 16:56:17 +0200

    removed unnecessary code in main

--- src/main.ino
+++ src/main.ino
@@ -6,38 +6,32 @@
 #include <string>
 #include <Streaming.h>
 #include <logger/telnetServer.h>
-#include <udpserver.h>
 #include <wifi.h>
 
 ADC_MODE(ADC_VCC);		// read internal adc
 Ticker msTicker;
 
-espLogger myLogger;
+espLogger myLogger("", []() {return millis();});
 
 void milisecondISR() {
 	static int run;
 	run++;
 	if (!(run % 1000)) {
 		myLogger << "test " << (int) run / 1000 << endl;
-//		myLogger << "test " << endl;
 	}
 }
 
 void setup() {
 	pinMode(2, OUTPUT);   // sets the pin as output
 	Serial.begin(115200);
-//	Serial << endl << "[SYS] Booting" << endl;
 	wifiSetup();
 	otaSetup();
 //	mqttSetup();
 	delay(1);
 	msTicker.attach(0.001, milisecondISR);
 	sei();
-//	telnetSetup();
 }
 
 void loop() {
 	otaLoop();
-//	telnet.loop();
-//	telnetLoop();
 }
